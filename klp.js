5053bc6657d03d3b7bcae26d5f4c8cf17c37bdec
tobey0852 <profit0852@gmail.com>
2023-11-21 13:19:01 +0100

    new

--- klp.js
+++ klp.js
@@ -1,15 +1,15 @@
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('cservice_3mymv5h', 'template_x6eb2df', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
+// window.onload = function() {
+//     document.getElementById('contact-form').addEventListener('submit', function(event) {
+//         event.preventDefault();
+//         // generate a five digit number for the contact_number variable
+//         this.contact_number.value = Math.random() * 100000 | 0;
+//         // these IDs from the previous steps
+//         emailjs.sendForm('cservice_3mymv5h', 'template_x6eb2df', this)
+//             .then(function() {
+//                 console.log('SUCCESS!');
+//             }, function(error) {
+//                 console.log('FAILED...', error);
+//             });
+//     });
+// }
